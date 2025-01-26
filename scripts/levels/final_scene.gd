72dc9f1c8195ac41d88f3e57af243a90917491cb
Rodrigo <freakrho@gmail.com>
2025-01-26 13:08:54 -0300

    New final scene

--- scripts/levels/final_scene.gd
+++ scripts/levels/final_scene.gd
@@ -1,9 +1,32 @@
-extends Level
+extends Node2D
 
 
 @export var thanks_for_playing_scene: PackedScene
+@export var wait_time: float = 3
 
+var ended := false
+var timer: Timer
 
+func _ready() -> void:
+    timer = Timer.new()
+    add_child(timer)
+    timer.wait_time = wait_time
+    timer.timeout.connect(_on_timeout)
+    timer.start()
+
+func _on_timeout():
+    timer.stop()
+    if ended:
+        end_game()
+    
 func end_game():
     get_tree().root.add_child(thanks_for_playing_scene.instantiate())
     queue_free()
+
+
+func _input(event: InputEvent) -> void:
+    if event.is_action("interact") or event is InputEventMouseButton:
+        if timer.is_stopped():
+            end_game()
+        else:
+            ended = true
