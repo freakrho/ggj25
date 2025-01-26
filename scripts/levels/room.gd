cdfaa74e160258ba4d7a944422405382c21baa96
Rodrigo <freakrho@gmail.com>
2025-01-26 10:25:40 -0300

    add mugs to room

--- scripts/levels/room.gd
+++ scripts/levels/room.gd
@@ -1,5 +1,13 @@
 extends Level
 
+@export var character_mugs: Array[CharacterPair]
+
+func _ready() -> void:
+    super()
+    for pair in character_mugs:
+        print(pair.character not in SessionManager.current.killed)
+        if pair.character not in SessionManager.current.killed:
+            pair.node.get_parent().remove_child(pair.node)
 
 func go_to_front():
     GameManager.load_level(LevelList.house)
