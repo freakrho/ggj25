ac405b40505ff94dd58d254c5018b5ca36977a74
Rodrigo <freakrho@gmail.com>
2025-01-26 10:52:10 -0300

    Changed coins system

--- scripts/levels/room.gd
+++ scripts/levels/room.gd
@@ -1,6 +1,7 @@
 extends Level
 
 @export var character_mugs: Array[CharacterPair]
+@export var coins_per_day: Array[Array]
 
 func _ready() -> void:
     super()
@@ -8,6 +9,8 @@ func _ready() -> void:
         print(pair.character not in SessionManager.current.killed)
         if pair.character not in SessionManager.current.killed:
             pair.node.get_parent().remove_child(pair.node)
+    # set coins
+    SessionManager.current.coins = coins_per_day[min(coins_per_day.size() -1, SessionManager.current.day)]
 
 func go_to_front():
     GameManager.load_level(LevelList.house)
--- scripts/set_coins_action.gd
+++ scripts/set_coins_action.gd
@@ -1,6 +1,14 @@
 extends Node
 
-@export var coins: Array[int]
 
 func set_coins():
-    SessionManager.current.coins = coins
+    # create enough change to have 3 $1 coins
+    var total = 0
+    for i in range(SessionManager.current.coins.size() - 1, -1, -1):
+        var coin = SessionManager.current.coins[i]
+        total += coin
+        SessionManager.current.coins.remove_at(i)
+        if total >= 3:
+            break
+    for i in range(total):
+        SessionManager.current.coins.append(1)
