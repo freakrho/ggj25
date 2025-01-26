df29fd404135af7d76fd340dcb78b4c373b1b1bc
Rodrigo <freakrho@gmail.com>
2025-01-26 11:37:03 -0300

    coffee minigame changes

--- scripts/drag_object_to_target_condition.gd
+++ scripts/drag_object_to_target_condition.gd
@@ -1,9 +1,15 @@
 class_name DraggObjectToTargetCondition extends MinigameCondition
 
+signal on_drop()
+
 @export var reset_if_failed: bool
 @export var draggable: Draggable
 @export var slideable: Slideable
 @export var target: Area2D
+@export var wait_time: float
+
+var timer: Timer
+var blocked := false
 
 func _ready() -> void:
     if reset_if_failed:
@@ -11,16 +17,31 @@ func _ready() -> void:
             slideable.dropped.connect(_on_drop)
         else:
             draggable.dropped.connect(_on_drop)
+    if timer == null:
+        timer = Timer.new()
+        add_child(timer)
+        timer.timeout.connect(_on_timeout)
 
 func _on_drop():
     if slideable != null:
         if !dependency_complete() or !target.overlaps_area(slideable.knob):
             slideable.reset()
+            return
     else:
         if !dependency_complete() or !target.overlaps_area(draggable):
             draggable.reset()
+            return
+    if wait_time > 0:
+        timer.start()
+        blocked = true
+    on_drop.emit()
 
 func condition_met() -> bool:
+    if blocked:
+        return false
     if slideable != null:
         return target.overlaps_area(slideable.knob) and not slideable.dragging
     return target.overlaps_area(draggable) and not draggable.dragging
+
+func _on_timeout():
+    blocked = false
--- scripts/minigame_conditions/press_button_condition.gd
+++ scripts/minigame_conditions/press_button_condition.gd
@@ -1,15 +1,31 @@
 class_name PressButtonCondition extends MinigameCondition
 
+signal on_pressed()
+
 @export var button: Button
+@export var wait_time: float
 
 var pressed := false
+var timer: Timer
 
 func _ready() -> void:
     button.pressed.connect(_on_pressed)
+    if timer == null:
+        timer = Timer.new()
+        add_child(timer)
+        timer.timeout.connect(_on_timeout)
 
 func _on_pressed():
     if dependency_complete():
-        pressed = true
+        if wait_time > 0:
+            timer.wait_time = wait_time
+            timer.start()
+        else:
+            pressed = true
+        on_pressed.emit()
+
+func _on_timeout():
+    pressed = true
 
 func condition_met():
     return pressed
