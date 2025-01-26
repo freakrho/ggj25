c17995e62c2e9297465c4b652f88769949b9f902
Rodrigo <freakrho@gmail.com>
2025-01-25 22:03:22 -0300

    Dialogue fix

--- scripts/dialogue_settings.gd
+++ scripts/dialogue_settings.gd
@@ -5,8 +5,11 @@ class_name DialogueSettings extends Node
 func start_dialogue():
     Dialogic.Styles.load_style(GlobalDialogueSettings.dialogue_style.name)
     var layout = Dialogic.start(dialogue)
+    register_characters.call_deferred(layout)
 
+func register_characters(layout):
     for character in GameManager.current_level.characters:
         layout.register_character(character.character, character.node)
+    
     layout.register_character(GameManager.player.dialogue_character,
         GameManager.player.dialogue_marker)
