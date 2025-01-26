ea4a1e931c4cffcf18cd786b390bc771a0acd589
Rodrigo <freakrho@gmail.com>
2025-01-26 01:12:49 -0300

    kill characters

--- scripts/choose_cup_condition.gd
+++ scripts/choose_cup_condition.gd
@@ -2,15 +2,23 @@ class_name ChooseCupCondition extends MinigameCondition
 
 @export var choices: Array[AreaSelectable]
 @export var characters: Array[DialogicCharacter]
-
+@export var cups_on_table: Array[Node2D]
 
 func _ready():
-    for choice in choices:
-        choice.connect("selected", on_selected)
+    for cup in cups_on_table:
+        cup.hide()
+    for i in range(characters.size()):
+        var choice = choices[i]
+        if characters[i] in SessionManager.current.killed:
+            choice.get_parent().remove_child(choice)
+        else:
+            choice.selected.connect(on_selected)
 
 func on_selected(selectable: AreaSelectable) -> void:
     completed = true
+    selectable.get_parent().remove_child(selectable)
     for i in range(choices.size()):
         if choices[i] == selectable:
             SessionManager.current.kill(characters[i])
+            cups_on_table[i].show()
             return
--- scripts/levels/office.gd
+++ scripts/levels/office.gd
@@ -2,7 +2,15 @@ extends Level
 
 @export var cup_dialogues: Array[DialogueSettings]
 @export var cup_owners: Array[DialogicCharacter]
+@export var character_nodes: Array[Node]
 
+
+func _ready() -> void:
+    super()
+    for i in range(cup_owners.size()):
+        if cup_owners[i] in SessionManager.current.killed:
+            var character_node = character_nodes[i]
+            character_node.get_parent().remove_child(character_node)
 
 func _on_coffee_minigame_ended(minigame: Minigame) -> void:
     minigame.close_minigame()
