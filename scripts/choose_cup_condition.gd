2ce1f9ffd1855ea952473889518667ff62dda678
Rodrigo <freakrho@gmail.com>
2025-01-26 14:58:08 -0300

    Fix coffee minigame

--- scripts/choose_cup_condition.gd
+++ scripts/choose_cup_condition.gd
@@ -4,7 +4,13 @@ class_name ChooseCupCondition extends MinigameCondition
 @export var characters: Array[DialogicCharacter]
 @export var cups_on_table: Array[Node2D]
 
+
+var selected: AreaSelectable
+var cup_parent: Node
+
+
 func _ready():
+    cup_parent = choices[0].get_parent()
     for cup in cups_on_table:
         cup.hide()
     for i in range(characters.size()):
@@ -17,8 +23,17 @@ func _ready():
 func on_selected(selectable: AreaSelectable) -> void:
     completed = true
     selectable.get_parent().remove_child(selectable)
+    selected = selectable
     for i in range(choices.size()):
         if choices[i] == selectable:
-            SessionManager.current.kill(characters[i])
             cups_on_table[i].show()
+        else:
+            if choices[i].get_parent() == null:
+                cup_parent.add_child(choices[i])
+            cups_on_table[i].hide()
+
+func do_effect(minigame: Minigame):
+    for i in range(choices.size()):
+        if choices[i] == selected:
+            SessionManager.current.kill(characters[i])
             return
